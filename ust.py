9434c1b39ade22fe1cfd81020fc4101110da04d0
n.vinay teja <83742721+vinay6004@users.noreply.github.com>
2025-01-01 20:23:35 +0530

    Add files via upload

---
+++ ust.py
@@ -0,0 +1,13 @@
+'''age = 45
+height= 4.3
+name="vinay"
+ismarried = True
+print(name,"age is",age,"height",height)'''
+email="ilearnexcel@gmail.com"
+position=email.find("@")
+print(position)
+dot = email[email.index('.com'):]
+print(dot)
+gmail = email[email.index('@')+1:email.index(".")]
+print(gmail)
+
