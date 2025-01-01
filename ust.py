8f82819a7d55ea4af3b47035cc327bf2ddb8c88b
n.vinay teja <83742721+vinay6004@users.noreply.github.com>
2025-01-01 20:31:47 +0530

    Add files via upload

--- ust.py
+++ ust.py
@@ -3,11 +3,13 @@ height= 4.3
 name="vinay"
 ismarried = True
 print(name,"age is",age,"height",height)'''
-email="ilearnexcel@gmail.com"
-position=email.find("@")
-print(position)
-dot = email[email.index('.com'):]
-print(dot)
-gmail = email[email.index('@')+1:email.index(".")]
-print(gmail)
+
+email="ilearniexcel@gmail.com"
+print("The position of @ is",email.find("@"))
+
+print("Extracting only.com from email is",email[-4:])
+
+print("Extracting gmail from the email is ",email[-9:-4])
+
+print("splitiing email based on @",email.split("@"))
 
